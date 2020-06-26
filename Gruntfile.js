340eda82c751b01abf8409dbcd210ef9d4baeb19
Hector Lopez <hlopezcarballo@gmail.com>
2020-06-26 07:43:47 -0500

    Initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+    //Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        watch: {
+            stylus: {
+                files: [
+                    'src/stylus/**/*.styl'
+                ],
+                tasks: ['stylus']
+            },
+            pug: {
+                files: [
+                    'src/pug/*.pug'
+                ],
+                tasks: ['pug']
+            },
+        },
+
+
+        stylus:{
+            compile: {
+                options: {
+                    compress: false,
+                    paths: ['src/stylus/'],
+                    import: [
+                        "nib/*"
+                    ],
+                },
+                files: {
+                        './dist/css/styles.css': ['src/stylus/*.styl', '!src/stylus/_*.styl']
+                },
+            },
+        },
+
+        pug:{
+            compile:{
+                options: {
+                    pretty:true,
+                },
+                files: {
+                    './dist/index.html' : ['src/pug/index.pug', '!src/pug/_*.pug']
+                },
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadNpmTasks('grunt-contrib-pug')
+}
